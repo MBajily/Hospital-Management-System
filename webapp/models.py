0f432aa1c4cf25fbc73a69149ab83e3cb2bc1183
Mohammed Elgaily <mbajily@gmail.com>
2022-10-16 14:18:45 +0200

    Add 'Patient' and 'Kin for patient' tables to database.

--- webapp/models.py
+++ webapp/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import Group
 
 
 #===============================================================
-#========================  Civil_Status  =============================
+#=====================  Civil Status  ==========================
 #===============================================================
 class Civil_Status(models.Model):
 	Genders = (
@@ -29,3 +29,47 @@ class Civil_Status(models.Model):
 		return self.full_name
 
 
+
+#===============================================================
+#========================  Patients  ===========================
+#===============================================================
+class Patient(models.Model):
+	id = models.AutoField(primary_key=True)
+	nationality_id = models.ForeignKey(Civil_Status, null=True, on_delete=models.SET_NULL)
+	phone = models.CharField(max_length=20)
+	email = models.EmailField(max_length=200, null=True)
+
+	def __str__(self):
+		return self.nationality_id
+
+
+
+#===============================================================
+#=====================  Kin of patient  ========================
+#===============================================================
+class Kin(models.Model):
+	Relationships = (
+		('Father', 'Father'),
+		('Mother', 'Mother'),
+		('Husband', 'Husband'),
+		('Wife', 'Wife'),
+		('Son', 'Son'),
+		('Daughter', 'Dauther'),
+		('Brother', 'Father'),
+		('Sister', 'Sister'),
+		('Uncle', 'Uncle'),
+		('aunt', 'aunt'),
+		('Grandfather', 'Grandfather'),
+		('grandmother', 'grandmother'),
+		('Step Father', 'Step Father'),
+		('Step Mother', 'Step Mother'),
+		)
+	id = models.AutoField(primary_key=True)
+	full_name = models.CharField(max_length=200)
+	address = models.CharField(max_length=1000, null=True)
+	phone = models.CharField(max_length=20)
+	patient = models.ForeignKey(Patient, null=True, on_delete=models.SET_NULL)
+	relationship = models.CharField(max_length=200, choices=Relationships)
+
+	def __str__(self):
+		return "{} for {}".format(self.full_name)
